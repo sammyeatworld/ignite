b57c4e61b5f819065cb77e837d736b4879514e49
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-10-05 14:47:21 +0200

    MachO: Add a new package

---
+++ Packages/IgniteMachO/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IgniteMachO",
+    products: [
+        .library(
+            name: "IgniteMachO",
+            type: .dynamic,
+            targets: ["IgniteMachO"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "IgniteMachO"
+        ),
+        .testTarget(
+            name: "IgniteMachOTests",
+            dependencies: ["IgniteMachO"]
+        ),
+    ]
+)
