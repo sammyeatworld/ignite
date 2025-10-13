5a405e55949fdc96e0cd22c4997b0c66726e197b
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-10-13 22:45:24 +0200

    Darwin: Add LayoutWrapper protocol for C to Swift bridging

---
+++ Packages/DarwinKit/Sources/Protocols/LayoutWrapper.swift
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2025, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import Foundation
+import Darwin
+
+@dynamicMemberLookup
+public protocol LayoutWrapper {
+    
+    associatedtype Layout
+    
+    var layout: Layout { get set }
+    
+}
+
+public extension LayoutWrapper {
+
+    subscript<Value>(dynamicMember keyPath: KeyPath<Layout, Value>) -> Value {
+        layout[keyPath: keyPath]
+    }
+
+}
+
+public extension LayoutWrapper {
+    
+    static var layoutSize: Int {
+        MemoryLayout<Layout>.size
+    }
+    
+    var layoutSize: Int {
+        MemoryLayout<Layout>.size
+    }
+    
+}
+
+public extension LayoutWrapper {
+    
+    static func layoutOffset(of key: PartialKeyPath<Layout>) -> Int {
+        MemoryLayout<Layout>.offset(of: key)!
+    }
+    
+    func layoutOffset(of key: PartialKeyPath<Layout>) -> Int {
+        MemoryLayout<Layout>.offset(of: key)!
+    }
+    
+}
