d54fd26147a08dd167317197a46667d5b4b853e5
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-10-18 23:47:04 +0200

    Darwin: Add type aliases

---
+++ Packages/DarwinKit/Sources/Models/Typealiases.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2025, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import Foundation
+
+public typealias PID = pid_t
+public typealias TaskHandle = mach_port_t
