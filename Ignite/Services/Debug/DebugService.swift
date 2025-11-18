45b35da8b11e30bf4b977784dbb912e8569f4be0
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-11-18 14:38:04 +0100

    Ignite: Add Debug service skeleton with API

---
+++ Ignite/Services/Debug/DebugService.swift
@@ -0,0 +1,130 @@
+//
+// Copyright (c) 2025, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import Foundation
+import FactoryKit
+import DebugKit
+
+// MARK: - Protocol
+
+protocol DebugServiceProtocol: AnyObject {
+  
+    // MARK: - Properties
+
+    // MARK: - Methods
+
+    func pauseExecution() async
+    func runToCursor() async
+    func runUntilReturn() async
+    func stepInto() async
+    func stepOver() async
+    func stepOut() async
+    func stepIntoInstruction() async
+    func stepOverInstruction() async
+    func stepIntoThread() async
+    func stepOverThread() async
+    func deactivateBreakpoints() async
+    func fetchBreakpoints() async
+    func attachToProcess(_ name: String?, pid: Int32?) async
+    func detachFromProcess() async
+    func debugExecutable(at path: URL) async
+
+}
+
+// MARK: - Implementation
+
+final actor DebugService: DebugServiceProtocol {
+   
+    // MARK: - Services
+
+    // MARK: - Properties
+
+    // MARK: - Initializer
+
+}
+
+// MARK: - Public Methods
+
+extension DebugService {
+    
+    func pauseExecution() async {
+        
+    }
+
+    func runToCursor() async {
+        
+    }
+
+    func runUntilReturn() async {
+        
+    }
+
+    func stepInto() async {
+        
+    }
+
+    func stepOver() async {
+        
+    }
+
+    func stepOut() async {
+        
+    }
+
+    func stepIntoInstruction() async {
+        
+    }
+
+    func stepOverInstruction() async {
+        
+    }
+
+    func stepIntoThread() async {
+        
+    }
+
+    func stepOverThread() async {
+        
+    }
+
+    func deactivateBreakpoints() async {
+        
+    }
+
+    func fetchBreakpoints() async {
+        
+    }
+
+    func attachToProcess(_ name: String?, pid: Int32?) async {
+        
+    }
+
+    func detachFromProcess() async {
+        
+    }
+
+    func debugExecutable(at path: URL) async {
+        
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension DebugService {
+    
+}
+
+// MARK: - Factory
+
+extension Container {
+    
+    var debugService: Factory<DebugServiceProtocol> {
+        self { @MainActor in DebugService() }
+            .cached
+    }
+    
+}
