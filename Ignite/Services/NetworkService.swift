8f0fd5aac9c721c76ade864ef76785d687845874
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-11-22 14:31:43 +0100

    Ignite: Add Network and Notification service skeletons

---
+++ Ignite/Services/NetworkService.swift
@@ -0,0 +1,53 @@
+//
+// Copyright (c) 2025, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import Foundation
+import FactoryKit
+
+// MARK: - Protocol
+
+protocol NetworkServiceProtocol: AnyObject {
+  
+    // MARK: - Properties
+
+    // MARK: - Methods
+
+}
+
+// MARK: - Implementation
+
+final class NetworkService: NetworkServiceProtocol {
+   
+    // MARK: - Services
+
+    // MARK: - Properties
+
+    // MARK: - Initializer
+
+}
+
+// MARK: - Public Methods
+
+extension NetworkService {
+
+}
+
+// MARK: - Private Methods
+
+private extension NetworkService {
+    
+}
+
+// MARK: - Factory
+
+extension Container {
+    
+    var networkService: Factory<NetworkServiceProtocol> {
+        self { NetworkService() }
+            .singleton
+    }
+    
+}
---
+++ Ignite/Services/NotificationService.swift
@@ -0,0 +1,53 @@
+//
+// Copyright (c) 2025, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import Foundation
+import FactoryKit
+
+// MARK: - Protocol
+
+protocol NotificationServiceProtocol: AnyObject {
+  
+    // MARK: - Properties
+
+    // MARK: - Methods
+
+}
+
+// MARK: - Implementation
+
+final class NotificationService: NotificationServiceProtocol {
+   
+    // MARK: - Services
+
+    // MARK: - Properties
+
+    // MARK: - Initializer
+
+}
+
+// MARK: - Public Methods
+
+extension NotificationService {
+
+}
+
+// MARK: - Private Methods
+
+private extension NotificationService {
+    
+}
+
+// MARK: - Factory
+
+extension Container {
+    
+    var notificationService: Factory<NotificationServiceProtocol> {
+        self { NotificationService() }
+            .singleton
+    }
+    
+}
