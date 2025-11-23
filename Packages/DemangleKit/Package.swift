daa2ba5aa8e6479509a68bf4fc8819ebb53c54e4
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2025-11-23 22:15:03 +0100

    Demangle: Add base files

---
+++ Packages/DemangleKit/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DemangleKit",
+    platforms: [.macOS(.v26)],
+    products: [
+        .library(
+            name: "DemangleKit",
+            type: .dynamic,
+            targets: ["DemangleKit"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../FoundationKit"),
+    ],
+    targets: [
+        .target(
+            name: "DemangleKit",
+            dependencies: [
+                .product(name: "FoundationKit", package: "FoundationKit"),
+            ],
+            swiftSettings: [
+                .strictMemorySafety(),
+                .swiftLanguageMode(.v6)
+            ]
+        ),
+        .testTarget(
+            name: "DemangleKitTests",
+            dependencies: ["DemangleKit"]
+        ),
+    ]
+)
